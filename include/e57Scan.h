a003965e2a8c6c40993d1417bc4847f4b82da95d
sariug <ugurcansari93@gmail.com>
2021-01-24 18:03:43 +0100

    added row col reading

--- include/e57Scan.h
+++ include/e57Scan.h
@@ -4,24 +4,44 @@
 #include <string>
 #include "E57SimpleReader.h"
 
-struct scanData
+struct ScanData
 {
     std::int64_t nCol, nRow, nTotalPoints, nGroupSize, nPointsPerGroup;
     bool colIndex = true;
-    e57::Data3DPointsData data;
+    std::vector<float> x, y, z, i;
+    std::vector<uint8_t> r, g, b;
+    std::vector<int8_t> invCart, invSphere;
+    std::vector<int32_t> row, col;
+
     e57::Data3D header;
+
+    double getIntensity(int index)
+    {
+        return (i[index] - header.intensityLimits.intensityMinimum) / (header.intensityLimits.intensityMaximum - header.intensityLimits.intensityMinimum);
+    }
+    int getRed(int index)
+    {
+        return ((r[index] - header.colorLimits.colorRedMinimum) * 255) / (header.colorLimits.colorRedMaximum - header.colorLimits.colorRedMinimum);
+    }
+    int getGreen(int index)
+    {
+        return ((g[index] - header.colorLimits.colorGreenMinimum) * 255) / (header.colorLimits.colorGreenMaximum - header.colorLimits.colorGreenMinimum);
+    }
+    int getBlue(int index)
+    {
+        return ((b[index] - header.colorLimits.colorBlueMinimum) * 255) / (header.colorLimits.colorBlueMaximum - header.colorLimits.colorBlueMinimum);
+    }
 };
 
 class e57Scan
 {
     e57::E57Root fileTopHeader;
-    std::vector<scanData> scans;
+    std::vector<ScanData> scans;
 
 public:
     e57Scan();
 
     int load(const std::string &filename);
     const e57::E57Root &getTopHeader() const { return fileTopHeader; };
-    const std::vector<scanData>  &getScans() const { return scans; };
-
+    const std::vector<ScanData> &getScans() const { return scans; };
 };
--- src/e57Scan.cpp
+++ src/e57Scan.cpp
@@ -7,73 +7,102 @@ e57Scan::e57Scan()
 
 int e57Scan::load(const std::string &filename)
 {
-    try
+    // try
+    // {
+    std::unique_ptr<e57::Reader> eReader = std::make_unique<e57::Reader>("pumpARowColumnIndex.e57");
+
+    if (!eReader->IsOpen())
+        return 1;
+
+    eReader->GetE57Root(fileTopHeader);
+
+    std::cout << filename << " is being loaded.\n";
+    std::cout << fileTopHeader.coordinateMetadata << std::endl;
+
+    scans.resize(eReader->GetData3DCount());
+    for (auto scanIdx = 0; scanIdx < scans.size(); scanIdx++)
     {
-        std::unique_ptr<e57::Reader> eReader = std::make_unique<e57::Reader>("pumpACartesian.e57");
-        
-        if(!eReader->IsOpen())
-            return 1;
+        eReader->ReadData3D(scanIdx, scans[scanIdx].header);
+        std::cout << scans[scanIdx].header.name << std::endl;
+        auto &scan = scans[scanIdx];
 
+        eReader->GetData3DSizes(scanIdx, scan.nRow, scan.nCol, scan.nTotalPoints,
+                                scan.nGroupSize, scan.nPointsPerGroup, scan.colIndex);
 
-        
-        eReader->GetE57Root(fileTopHeader);
-
-        std::cout << filename << " is being loaded.\n";
-        std::cout << fileTopHeader.coordinateMetadata << std::endl;
-
-        scans.resize(eReader->GetData3DCount());
-        for (auto scanIdx= 0; scanIdx < scans.size(); scanIdx++)
+        // bufer size check and update(http://www.libe57.org/TutorialSimpleAPI.html)
+        int64_t bufferSize = (scan.nRow > 0) ? scan.nRow : 1024;
+        std::cout << "buffer size" << bufferSize << " " << scan.nRow << std::endl;
+        const auto &availableFields = scan.header.pointFields;
+        const bool cartesianAvailable = availableFields.cartesianXField && availableFields.cartesianYField && availableFields.cartesianZField;
+        const bool colorAvailable = availableFields.colorRedField && availableFields.colorGreenField && availableFields.colorBlueField;
+        const bool intensityAvailable = availableFields.intensityField;
+        if (cartesianAvailable)
         {
-            eReader->ReadData3D(scanIdx, scans[scanIdx].header);
-            std::cout << scans[scanIdx].header.name << std::endl;
-
-            scanData s;
-            eReader->GetData3DSizes(scanIdx, s.nRow, s.nCol, s.nTotalPoints,
-                                   s.nGroupSize, s.nPointsPerGroup, s.colIndex);
-
-            // bufer size check and update(http://www.libe57.org/TutorialSimpleAPI.html)
-            int64_t bufferSize = (s.nRow > 0) ? s.nRow : 1024;	
-
-
-
-
+            scan.x.reserve(bufferSize);
+            scan.y.reserve(bufferSize);
+            scan.z.reserve(bufferSize);
+            if (availableFields.cartesianInvalidStateField)
+                scan.invCart.reserve(bufferSize);
         }
+        if (colorAvailable)
+        {
+            scan.r.reserve(bufferSize);
+            scan.g.reserve(bufferSize);
+            scan.b.reserve(bufferSize);
+        }
+        if (intensityAvailable)
+            scan.i.reserve(bufferSize);
 
+        if (availableFields.rowIndexField)
+            scan.row.reserve(bufferSize);
+        if (availableFields.columnIndexField)
+            scan.col.reserve(bufferSize);
+        e57::Data3DPointsData buffer;
+        buffer.cartesianX = scan.x.data();
+        buffer.cartesianY = scan.y.data();
+        buffer.cartesianZ = scan.z.data();
+        buffer.colorRed = scan.r.data();
+        buffer.colorGreen = scan.g.data();
+        buffer.colorBlue = scan.b.data();
+        buffer.intensity = scan.i.data();
+        buffer.columnIndex = scan.col.data();
+        buffer.rowIndex = scan.row.data();
+        buffer.cartesianInvalidState = scan.invCart.data();
+        buffer.sphericalInvalidState = scan.invSphere.data();
 
-            // Reader::GetData3DCount() const
+        // Setup buffers
+        e57::CompressedVectorReader dataReader = eReader->SetUpData3DPointsData(scanIdx, bufferSize, buffer);
 
-     
-            // bool
-            // Reader::GetData3DSizes(int64_t dataIndex, int64_t & rowMax, int64_t & columnMax, int64_t & pointsSize,
-            //                        int64_t & groupsSize, int64_t & countSize, bool &bColumnIndex) const
+        int64_t count = 0;
+        unsigned size = 0;
+        int32_t col = 0, row = 0;
 
-            // bool
-            // Reader::ReadData3DGroupsData(int64_t dataIndex, int64_t groupCount, int64_t * idElementValue,
-            //                              int64_t * startPointIndex, int64_t * pointCount) const
+        while (size = dataReader.read())
+        {
+            for (long i = 0; i < size; i++)
+            {
 
-            // CompressedVectorReader
-            // Reader::SetUpData3DPointsData(int64_t dataIndex, size_t pointCount,
-            //                               const Data3DPointsData &buffers) const
+                if (scan.header.pointFields.columnIndexField)
+                    col = scan.col[i];
+                else
+                    col = 0; //point cloud case
 
-            // CompressedVectorReader
-            // Reader::SetUpData3DPointsData(int64_t dataIndex, size_t pointCount,
-            //                               const Data3DPointsData_d &buffers) const
-    
-        
-            // int64_t
-            // Reader::GetImage2DCount() const bool
-            // Reader::ReadImage2D(int64_t imageIndex, Image2D & image2DHeader) const bool
-            // Reader::GetImage2DSizes(int64_t imageIndex, Image2DProjection & imageProjection, Image2DType & imageType,
-            //                         int64_t & imageWidth, int64_t & imageHeight, int64_t & imageSize,
-            //                         Image2DType & imageMaskType, Image2DType & imageVisualType) const int64_t
-            // Reader::ReadImage2DData(int64_t imageIndex, Image2DProjection imageProjection, Image2DType imageType,
-            //                         void *pBuffer, int64_t start, int64_t count) const int64_t
-        
-        
-        eReader->Close();
+                if (scan.header.pointFields.rowIndexField)
+                    row = scan.row[i];
+                else
+                    row = count; //point cloud case
+
+                    std::cout << scan.row[i]<< " " << col << " " << scan.x[i] << " " << scan.y[i] << " " << scan.z[i] << std::endl;
+
+                count++;
+            }
+        }
     }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+
+    eReader->Close();
+    // }
+    // catch (const std::exception &e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
 }
